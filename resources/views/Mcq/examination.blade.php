71f694bf1a1bc1e5274ae420d1293250b07097aa
psbappa <bappadey113063@gmail.com>
2021-06-04 13:25:38 +0530

    modified

--- resources/views/Mcq/examination.blade.php
+++ resources/views/Mcq/examination.blade.php
@@ -12,19 +12,18 @@
       foreach($get_questions as $key=>$questions) {
         $options = explode(",",$questions->options);
     ?>
-    <p><strong>Q<?php echo $key+1;?> .</strong><?php echo $questions->question?></p>
+    <label><p><strong>Q<?php echo $key+1;?> .</strong><?php echo $questions->question?></p></label>
     <?php
       foreach($options as $key=>$option) {
     ?>
     <div class="question">
-        <label><input required type="radio" name=<?php echo $questions->id;?> value=<?php echo $key+1;?>><?php echo $option;?></label>
+      <input required type="radio" id="radio_option" name=<?php echo $questions->id;?> value=<?php echo $key+1;?>><?php echo $option;?>
     </div>
     <?php
       }}
     ?>
     <input type="submit" />
 </form>
-
 @endsection
 
 <link rel="stylesheet" href="{{ url('/') }}/assets/css/customStyle.css">
